1fc10bbb13f6230aa9119d121cc2444960165470
Zeel Chotaliya <134128833+ZeelVC@users.noreply.github.com>
2024-06-26 23:50:04 +0530

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -34,7 +34,7 @@ const Nav = () => {
                 return (
                     <Link 
                         href={link.path} 
-                        kay={index} 
+                        key={index} 
                         className={`${
                             link.path === pathname && "text-accent border-b-2 border-accent"
                         } capitalize font-medium hover:text-accent trasition-all`}
